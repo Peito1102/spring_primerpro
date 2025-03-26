534150147f5946f69c9d4387b8e6acfbc4e1dad0
Peito1102 <renzitiwin@gmail.com>
2025-03-25 21:27:00 -0500

    desde parte 14 a 16 (mvc 02)

--- src/main/java/com/vasquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/vasquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.vasquez.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.vasquez.curso.springboot.webapp.springboot_web.models.User;
 
@@ -17,4 +23,23 @@ public class UserController {
         model.addAttribute("user", user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model)  { 
+        
+
+        //model.addAttribute("users", users);
+        model.addAttribute("title","Listado de usuarios!");
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> userModel() {
+        List<User> users = Arrays.asList(new User("Joel","Aguilar","ja@hotmail.com"),
+            new User("Thomaz", "Vasquez","tva@hotmail.com"), new User("Piero", "Mendieta","pme@hotmail.com"),
+            new User("Lizu", "Loayza"));
+
+        return users;
+    }
+
 }
